04b9922ed7983d2554903fa25b1ccaf46ee1b8be
Yusuf KAYA <yusuf.kaya1059@gmail.com>
2024-02-20 19:05:16 +0300

    home section and about section fixed

--- gsap.js
+++ gsap.js
@@ -1,24 +1,59 @@
-
-// navbar animation
-const navbarLinks =document.querySelector('.navbar-nav');
-const circleImg = document.querySelector('.circle-container');
-const information = document.querySelector('.information');
-
-gsap.from(navbarLinks, {
+gsap.from('#navbar', {
     y: -200,
-    opacity: 0,
-    duration: 2,
-    delay: 1
+    duration: 1.5,
 })
 
-gsap.from(circleImg, {
-    x:-500,
+const generalObj = {
     opacity: 0,
     duration: 1,
+}
+
+gsap.from('.circle-container', {
+    x: -600,
+    ...generalObj,
+    scrollTrigger: {
+        trigger: '.circle-container',
+        start: "bottom 100%",
+        end: "bottom 0%",
+        toggleActions: "restart reverse restart reverse",
+    }
 })
-gsap.from(information, {
-    x:-1000,
-    opacity: 0,
-    duration: 1,
-    delay:1
+
+gsap.from('.home-information', {
+    x: 600,
+    ...generalObj,
+    scrollTrigger: {
+        trigger: '.home-information',
+        start: "bottom 100%",
+        end: "bottom 0%",
+        toggleActions: "restart reverse restart reverse",
+    }
 })
+
+gsap.from('.information', {
+    y: 100,
+    ...generalObj,
+    scrollTrigger: {
+        trigger: '.information',
+        start: "top 100%",
+        end: "top 0%",
+        toggleActions: "restart reverse restart reverse",
+    }
+})
+
+gsap.from('.skills', {
+    y: 100,
+    ...generalObj,
+    scrollTrigger: {
+        trigger: '.skills',
+        start: "top 100%",
+        end: "top 0%",
+        toggleActions: "restart reverse restart reverse",
+    }
+
+})
+
+
+
+// scrollTrigger
+
